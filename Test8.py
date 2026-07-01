388d8a8af806d375680b3e360dc65c5f6a26383a
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-07-01 15:14:50 -0700

    typos

--- Test8.py
+++ Test8.py
@@ -3,8 +3,8 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 
 with conn:
-    our = conn.cursor()
-    our.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
         col_fname TEXT, \
         col_lname TEXT, \
