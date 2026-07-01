1a69c0fea251c73da083b03594b053ee134d3285
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-07-01 15:48:10 -0700

    OOP

---
+++ Object Oriented Programing.py
@@ -0,0 +1,9 @@
+# Class (blueprint)
+class Player:
+    def __init__(self, gold, level):
+        self.gold = gold
+        self.level = level
+
+# Objects
+player1 = Player(6, 3)
+player2 = Player(15, 35)
