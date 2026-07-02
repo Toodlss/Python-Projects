19a50b49aef610d6e7a28197d774ff3bce26a155
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-07-01 18:03:18 -0700

    Update Object Oriented Programing.py

--- Object Oriented Programing.py
+++ Object Oriented Programing.py
@@ -4,6 +4,20 @@ class Player:
         self.gold = gold
         self.level = level
 
+
+# Character classes
+class Warrior(Player):
+    def __init__(self, gold, level, health, manna):
+        super().__init__(gold, level)
+        self.health = health
+        self.manna = manna
+
+class Mage(Player):
+    def __init__(self, gold, level, health, manna):
+        super().__init__(gold, level)
+        self.health = health
+        self.mana = manna
+
 # Objects
-player1 = Player(6, 3)
-player2 = Player(15, 35)
+player1 = Warrior(2, 3, 100, 50)
+player2 = Mage(3, 35, 50, 100)
