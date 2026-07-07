8496d1e27acae9f54a6b8f9ea8612a0020e292ce
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-07-06 20:46:25 -0700

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,21 +1,25 @@
 class Health:
     def __init__(self):
+        # Private attribute because of the __ that sets health
         self.__health = 1000
 
     def getHealth(self):
         return self.__health
 
     def setHealth(self, amount):
-        self.__health = amount
+        #changes the private health attribute
+        self.__health -= amount
+        print(self.__health)
 
+        #prints if players health is at or below 0
         if self.__health <= 0:
             print("Player dies")
-
 
 player = Health()
 
 player.setHealth(500)
-print(player.getHealth())
 
-player.setHealth(0)
-print(player.getHealth())
+#kills player
+player.setHealth(500)
+
+
